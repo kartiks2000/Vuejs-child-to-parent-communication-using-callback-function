3bbb059e32d5245e511a94a7343ed997fe7844c9
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-07-17 18:02:31 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="kk">
+    <h1>Hello!! I am the parent....</h1>
+    <h2>Name is: {{childname}}</h2>
+    <Child :childname="childname" :chg="change"></Child>
+  </div>
+</template>
+
+<script>
+import child from './child';
+export default {
+  components:{
+    Child : child
+  },
+  data: function(){
+    return{
+      childname : "kartik"
+    }
+  },
+  methods:{
+    change(){
+      console.log("Parenting executing...");
+      this.childname="Aishwary";
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h1{
+    color: red;
+  }
+  div{
+    margin: 3rem;
+  }
+  .kk{
+    background-color: cornsilk;
+    padding: 3rem;
+  }
+</style>>
+
+</style>
---
+++ src/child.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Hello!! I am the child....</h1>
+    <h2>my name is {{childname}} .</h2>
+    <button @click="changename">Change name</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["childname","chg"],
+  methods:{
+      changename(){
+          this.chg();
+      }
+  }
+}
+</script>
+
+<style scoped>
+    h1{
+        color: blue;
+    }
+    div{
+        background-color: burlywood;
+        padding: 2rem;
+    }
+</style>>
+
